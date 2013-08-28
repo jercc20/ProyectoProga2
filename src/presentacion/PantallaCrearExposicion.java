84504b69a38e809031b7ea36951dd840714390bd
mauriciofer <mauriciofer@gmail.com>
2013-08-27 23:48:07 -0600

    Acomodo pantallaCrearExpo

--- src/presentacion/PantallaCrearExposicion.java
+++ src/presentacion/PantallaCrearExposicion.java
@@ -2,23 +2,36 @@ package presentacion;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import logica.DAOGaleria;
 import logica.Galeria;
+import javax.swing.JList;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PantallaCrearExposicion extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtIdGaleria;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtFechaInicio;
+	private JTextField txtFechaFinal;
+	private List listPinturas;
+	private JLabel lblPinturasAExponer;
+	private List listPorExponer;
+	private JButton btnAgregar;
+	private JButton btnCancel;
+	private JButton btnCrear;
+	private JButton btnRemover;
 
 	/**
 	 * Create the frame.
@@ -29,7 +42,7 @@ public class PantallaCrearExposicion extends JFrame {
 		
 		setTitle("Crear Exposici\u00F3n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 368, 260);
+		setBounds(100, 100, 430, 392);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -37,40 +50,99 @@ public class PantallaCrearExposicion extends JFrame {
 		
 		JLabel lblIdGaleria = new JLabel("Id Galeria");
 		lblIdGaleria.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblIdGaleria.setBounds(44, 7, 96, 14);
+		lblIdGaleria.setBounds(10, 13, 96, 14);
 		contentPane.add(lblIdGaleria);
 		
 		txtIdGaleria = new JTextField();
-		txtIdGaleria.setBounds(150, 5, 151, 17);
+		txtIdGaleria.setBounds(116, 11, 99, 17);
 		contentPane.add(txtIdGaleria);
 		txtIdGaleria.setColumns(10);
 		txtIdGaleria.setText(galeria.getId());
 		txtIdGaleria.setEnabled(false);
 		
-		textField = new JTextField();
-		textField.setBounds(150, 26, 151, 17);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtFechaInicio = new JTextField();
+		txtFechaInicio.setBounds(116, 32, 99, 17);
+		contentPane.add(txtFechaInicio);
+		txtFechaInicio.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(150, 47, 151, 17);
-		contentPane.add(textField_1);
+		txtFechaFinal = new JTextField();
+		txtFechaFinal.setColumns(10);
+		txtFechaFinal.setBounds(116, 53, 99, 17);
+		contentPane.add(txtFechaFinal);
 		
 		JLabel lblFechaInicio = new JLabel("Fecha Inicio");
 		lblFechaInicio.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblFechaInicio.setBounds(54, 28, 86, 14);
+		lblFechaInicio.setBounds(20, 34, 86, 14);
 		contentPane.add(lblFechaInicio);
 		
 		JLabel lblFechaFinal = new JLabel("Fecha Final");
 		lblFechaFinal.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblFechaFinal.setBounds(54, 49, 86, 14);
+		lblFechaFinal.setBounds(20, 55, 86, 14);
 		contentPane.add(lblFechaFinal);
 		
 		JLabel lblListaPinturas = new JLabel("Lista Pinturas");
 		lblListaPinturas.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblListaPinturas.setBounds(44, 70, 96, 14);
+		lblListaPinturas.setBounds(10, 80, 96, 14);
 		contentPane.add(lblListaPinturas);
+		
+		listPinturas = new List();
+		listPinturas.setBounds(116, 75, 235, 90);
+		contentPane.add(listPinturas);
+		
+		lblPinturasAExponer = new JLabel("Pinturas a Exponer");
+		lblPinturasAExponer.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPinturasAExponer.setBounds(10, 180, 96, 14);
+		contentPane.add(lblPinturasAExponer);
+		
+		listPorExponer = new List();
+		listPorExponer.setBounds(116, 179, 235, 100);
+		contentPane.add(listPorExponer);
+		
+		btnAgregar = new JButton("Agregar");
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+			}
+		});
+		btnAgregar.setBounds(20, 115, 89, 23);
+		contentPane.add(btnAgregar);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(true);
+			}
+		});
+		btnCancel.setBounds(216, 319, 89, 23);
+		contentPane.add(btnCancel);
+		
+		btnCrear = new JButton("Crear");
+		btnCrear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Gestor.crearExposicion( txtIdGaleria.getText(), txtFechaInicio.getText(), txtFechaFinal.getText()/*aca faltaria la lista de pinturas*/);
+					JOptionPane.showMessageDialog( null, "El mecenazgo ha sido creado exitosamente!" );
+					setVisible(false);
+				} catch (Exception e1) {
+					JOptionPane.showMessageDialog( null, "Hubo un error\nPor favor revise los datos ingresados");
+				}
+			}
+		});
+		btnCrear.setBounds(315, 319, 89, 23);
+		contentPane.add(btnCrear);
+		
+		btnRemover = new JButton("Remover");
+		btnRemover.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				
+			}
+		});
+		btnRemover.setBounds(17, 215, 89, 23);
+		contentPane.add(btnRemover);
 	}
-
 }
